af36ebb0271ec2ba912d1af4f4736ecaa3adc5d2
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-03 15:29:42 +0530

    Create Areaofroom.cpp

---
+++ Expressions/Areaofroom.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float width,length,area;
+	cout<<"Enter width and  length of a room"<<"\n";
+	cin>>width>>length;
+	area=width*length;
+	cout<<"Area of the room is "<<area<<" meters";
+}
