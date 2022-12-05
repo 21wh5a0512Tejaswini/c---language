4cfa64b3a35a8f65753fa0396e7d36cb977ed8c1
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-05 17:06:03 +0530

    Create Area_and_volume.cpp

---
+++ Expressions/Area_and_volume.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float r,area,volume;
+	cout<<"Enter radius  ";
+	cin>>r;
+	area=(22/7)*r*r;
+	volume=(4/3)*(22/7)*r*r*r;
+	cout<<area<<" "<<volume;
+}
