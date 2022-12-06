38a9becb9ba029a4838dc4c55405f928ea431f6e
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-06 16:15:19 +0530

    Update compound_interest.cpp

--- Expressions/compound_interest.cpp
+++ Expressions/compound_interest.cpp
@@ -6,8 +6,8 @@ int main()
 	float first=0.0,second=0.0,third=0.0;
 	cout<<"Enter amount of money deposited into the account:";
 	cin>>amount;
-	first=(float)4/100+amount;
-	second=(float)4/100+first;
-	third=(float)4/100+second;
-	cout<<first<<" "<<second<<" "<<third;	
+	first=(float)amount*4/100+amount;
+	second=(float)first*4/100+first;
+	third=(float)second*4/100+second;
+	cout<<fixed<<setprecision(2)<<first<<" "<<fixed<<setprecision(2)<<second<<" "<<fixed<<setprecision(2)<<third;	
 }
