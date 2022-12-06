10c356de69e15b02bf883f574bb92af27d844333
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-06 16:08:15 +0530

    Create Even_or_odd.cpp

---
+++ Decision_Making/Even_or_odd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int a;
+   cout<<"Enter an integer:";
+   cin>>a;
+   if(a%2==0){
+   	cout<<"Even";
+   }
+	else{
+		cout<<"Odd";
+	}
+}
