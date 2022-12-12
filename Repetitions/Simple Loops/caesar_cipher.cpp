182ec37b1a74674de8351750d1ee440d5add6997
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-12 15:59:44 +0530

    Update caesar_cipher.cpp

--- Repetitions/Simple Loops/caesar_cipher.cpp
+++ Repetitions/Simple Loops/caesar_cipher.cpp
@@ -2,17 +2,26 @@
 using namespace std;
 int main()
 {
-	string message,upper,lower;
+	string message,result,result1;
+	int upper,lower;
 	int i,shftamt;
 	cin>>message>>shftamt;
 	for( i=0;i<message.length();i++){
 		if(message[i]>='A' && message[i]<='Z'){
-		upper=(message[i]-'A'+shftamt)%26+'A';
-		cout<<upper;
+		upper=(message[i]-'A'+shftamt)%26;
+		if(upper<0){
+			upper=(upper+26)%26;	
+		}
+		result=upper+'A';
+		cout<<result;
 	}
 		else {
-			lower=(message[i]-'a'+shftamt)%26+'a';
-		    cout<<lower;
+		    lower=(message[i]-'a'+shftamt)%26;
+		if(lower<0){
+			lower=(lower+26)%26;	
+		}
+		result1=lower+'a';
+		cout<<result1;
 		}
 	}
 }
