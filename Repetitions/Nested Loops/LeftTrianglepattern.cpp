43dcc3440a0d4132a4dcde23e06862a3bcb1eca0
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-14 16:06:32 +0530

    Update LeftTrianglepattern.cpp

--- Repetitions/Nested Loops/LeftTrianglepattern.cpp
+++ Repetitions/Nested Loops/LeftTrianglepattern.cpp
@@ -5,11 +5,11 @@ int main()
 	int n;
 	cin>>n;
 	for(int i=1;i<=n;i++){
-		for(int k=n;k>=i;k--){
-			cout<<" ";
+		for(int k=n-1;k>=i;k--){
+			cout<<"   ";
 		}
 		for(int j=1;j<=i;j++){
-			cout<<"*";
+			cout<<" * ";
 		}	
 		cout<<"\n";
 		
