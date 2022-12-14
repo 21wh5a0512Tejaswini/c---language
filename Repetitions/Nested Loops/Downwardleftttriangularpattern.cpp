883920a20a99150bbd18cbf727c230dfe3f63047
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-14 16:05:48 +0530

    Update Downwardleftttriangularpattern.cpp

--- Repetitions/Nested Loops/Downwardleftttriangularpattern.cpp
+++ Repetitions/Nested Loops/Downwardleftttriangularpattern.cpp
@@ -6,10 +6,10 @@ int main()
 	cin>>n;
 	for(int i=1;i<=n;i++){
 		for(int k=1;k<=i-1;k++){
-			cout<<" ";	
+			cout<<"   ";	
 		}
 		for(int j=n;j>=i;j--){
-			cout<<"*";
+			cout<<" * ";
 		}
 		cout<<"\n";
 	}
