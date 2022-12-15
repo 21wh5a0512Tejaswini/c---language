540c19f4ec6e1578d455b19a68069fd535a9e85d
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-15 22:32:34 +0530

    Update ComputeHypotenuse.cpp

--- Functions/functions-1/ComputeHypotenuse.cpp
+++ Functions/functions-1/ComputeHypotenuse.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 float getHypotenuse(float x,float y){
-	 cout<<sqrt((x*x)+(y*y));	
+	 return sqrt((x*x)+(y*y));	
 }
 int main(){
-	float side1,side2;
+	float side1,side2,result;
 	cin>>side1>>side2;
-	getHypotenuse(side1,side2);
+	result=getHypotenuse(side1,side2);
+	cout<<result;
 }
