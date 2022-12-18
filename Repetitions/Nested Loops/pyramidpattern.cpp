b4b44cdfc7e6f15f0bacfad2c7c43b84b849e5b0
Rampe Tejaswini <92857583+21wh5a0512Tejaswini@users.noreply.github.com>
2022-12-18 20:36:51 +0530

    Update pyramidpattern.cpp

--- Repetitions/Nested Loops/pyramidpattern.cpp
+++ Repetitions/Nested Loops/pyramidpattern.cpp
@@ -4,12 +4,12 @@ int main()
 {
 	int n;
 	cin>>n;
-	for(int i=0;i<n;i++){
-		for(int k=n;k>=i;k--){
-			cout<<" ";
+	for(int i=1;i<=n;i++){
+		for(int k=1;k<=n-i;k++){
+			cout<<"  " ;
 		}
-		for(int j=1;j<=2*i+1;j++){
-			cout<<"*";
+		for(int j=1;j<=2*i-1;j++){
+			cout<<"* ";
 		}
 		cout<<"\n";			
 	}	
